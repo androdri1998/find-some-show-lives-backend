b700d0ae3f180ca48a48a8e27ea5a40a62ba3952
Andre Rodrigues <andre.rodrigues98@outlook.com>
2020-04-18 23:47:51 -0300

    inserindo melhorias no config database

--- backend/config/database.example.js
+++ backend/config/database.example.js
@@ -1,10 +1,16 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+});
+
 module.exports = {
   username: [username],
   password: [password],
   database: [name_database],
   host: [host],
   dialect: [dialect],
+  storage: "__tests__/database.sqlite",
   define: {
-    underscored: true
+    underscored: true,
+    underscoredAll: true
   }
 }
