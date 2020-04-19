652594f9a902fdf9773c1eed3f6186697911dfa0
Andre Rodrigues <andre.rodrigues98@outlook.com>
2020-04-19 11:22:29 -0300

    inserindo middleware de autenticaçao

--- backend/__tests__/integration/user.test.js
+++ backend/__tests__/integration/user.test.js
@@ -19,17 +19,57 @@ describe('Users', () => {
 
     const userCreated = await createUsersService(user);
 
+    const responseAuth = await request(app)
+      .post('/users/auth')
+      .send({
+        email: user.email,
+        password: user.password
+      });
+
     const response = await request(app)
-      .get(`/users/${userCreated.id}`);
+      .get(`/users/${userCreated.id}`)
+      .set('Authorization', `Bearer ${responseAuth.body.access_token}`);
 
     expect(response.status).toBe(200);
   });
 
-  it('should return error not found user', async () => {
-    const uuidUser = "3a71f69a-7c35-48cc-8a1b-9609fda1756d";
+  it('should to create a user in application and return error authorization get data of user', async () => {
+    const userPass = faker.internet.password();
+    const user = {
+      email: faker.internet.email(),
+      name: faker.name.findName(),
+      password: userPass
+    };
+
+    const userCreated = await createUsersService(user);
 
     const response = await request(app)
-      .get(`/users/${uuidUser}`);
+      .get(`/users/${userCreated.id}`);
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should return error not found user', async () => {
+    const uuidUser = "3a71f69a-7c35-48cc-8a1b-9609fda1756d";
+    const userPass = faker.internet.password();
+    const user = {
+      email: faker.internet.email(),
+      name: faker.name.findName(),
+      password: userPass
+    };
+
+    await createUsersService(user);
+
+    const responseAuth = await request(app)
+      .post('/users/auth')
+      .send({
+        email: user.email,
+        password: user.password
+      });
+
+    const response = await request(app)
+      .get(`/users/${uuidUser}`)
+      .set('Authorization', `Bearer ${responseAuth.body.access_token}`);
 
     expect(response.status).toBe(404);
     expect(response.body).toHaveProperty("error");
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,11 +2,14 @@ const routes = require('express').Router();
 const usersController = require('../app/controllers/users.controller');
 
 const validateParams = require('../app/middlewares/validateParams');
+const authMiddleware = require('../app/middlewares/auth');
 
 const { createUserSchema, authenticateUserSchema, getUserSchema } = require('../app/schemas/users.schema');
 
 routes.post('/', validateParams(createUserSchema, 'body'), usersController.createUsers);
 routes.post('/auth', validateParams(authenticateUserSchema, 'body'), usersController.authenticateUser);
+
+routes.use(authMiddleware);
 routes.get('/:user_id', validateParams(getUserSchema, 'params'), usersController.getUser);
 
 module.exports = routes;
